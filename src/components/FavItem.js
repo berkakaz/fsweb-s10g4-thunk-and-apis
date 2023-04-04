7794d2e438adc80c4cb4c83e1405a5add75f5a99
berkakaz <berk.akaz@gmail.com>
2023-04-04 14:56:30 +0300

    güncellendi

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { removeFav } from "../actions";
+import { useDispatch } from "react-redux";
 
-function FavItem({ title }) {
+function FavItem({ title, output }) {
+  const dispatch = useDispatch();
+
+  function handleRemove(a) {
+    dispatch(removeFav(a));
+  }
   return (
     <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
       <div className="flex-1 pr-4">{title}</div>
       <button
-        onClick={() => { }}
+        onClick={() => handleRemove(output)}
         className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
       >
         Çıkar
@@ -15,3 +22,4 @@ function FavItem({ title }) {
 }
 
 export default FavItem;
+
